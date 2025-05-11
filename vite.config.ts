b932be566314b9d73de8f9e374c6a28ada1519d8
anjish <40964308+anji5h@users.noreply.github.com>
2025-05-11 19:37:29 +0300

    removed vuetify and added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
+  plugins: [vue()],
 });
