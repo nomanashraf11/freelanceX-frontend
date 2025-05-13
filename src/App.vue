6a5db75d5b272eeb5ba38fdb32d6d210ca6bd765
muradmisu663@gmail.com <muradmisu663@gmail.com>
2025-05-13 16:59:29 +0300

    with footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="min-h-screen">
-    <router-view />
+  <div class="flex flex-col min-h-screen">
+    <!-- Your header is probably inside each page, or you can move it here -->
+    <router-view class="flex-grow" />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+import Footer from './components/footer.vue'
 </script>
